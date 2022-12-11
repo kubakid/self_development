5df14dc15b90711d75cad5265b091aa5fb83fec1
JK <kuba.kidon@wp.pl>
2022-12-11 20:47:09 +0100

    JK

--- src/highAndLow/Main.java
+++ src/highAndLow/Main.java
@@ -1,4 +1,16 @@
 package highAndLow;
 
 public class Main {
+    public static String highAndLow(String numbers) {
+        String[] num = numbers.split(" ");
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        for (String i : num){
+            if (Integer.parseInt(i) > max)
+                max = Integer.parseInt(i);
+            if (Integer.parseInt(i) < min)
+                min = Integer.parseInt(i);
+        }
+        return max + " " + min;
+    }
 }
